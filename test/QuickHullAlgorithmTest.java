8c45ae5a54fa326a56208065d6db86981af70edb
silent <hszwnts@gmx.com>
2014-04-12 19:35:48 +0200

    Added a new test for the QuickHull algorithm.

---
+++ test/QuickHullAlgorithmTest.java
@@ -0,0 +1,97 @@
+/**
+    Copyright (c) 2014 Patrick Hillert <silent@gmx.biz>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation; version 3 of the License.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+import static org.junit.Assert.*;
+
+import java.util.ArrayList;
+
+import main.Point;
+import main.QuickHullAlgorithm;
+import org.junit.Test;
+
+/**
+ * Test class for the QuickHull algorithm.
+ */
+public class QuickHullAlgorithmTest {
+
+    /**
+     * Test if three points placed as a triangle return
+     * three hull points.
+     */
+    @Test
+    public final void check2DForThreeHullPoints() {
+        ArrayList<Point> points = new ArrayList<Point>();
+
+        points.add(new Point(0, 0));
+        points.add(new Point(1, 0));
+        points.add(new Point(1, 1));
+
+        QuickHullAlgorithm qh = new QuickHullAlgorithm(points);
+        ArrayList<Point> outPoints = qh.getHullPoints();
+
+        assertEquals(3, outPoints.size());
+    }
+
+    /**
+     * Test if four points placed as a rectangle return
+     * four hull points.
+     */
+    @Test
+    public final void check2DForFourHullPoints() {
+        ArrayList<Point> points = new ArrayList<Point>();
+
+        points.add(new Point(0, 0));
+        points.add(new Point(1, 0));
+        points.add(new Point(1, 1));
+        points.add(new Point(0, 1));
+
+        QuickHullAlgorithm qh = new QuickHullAlgorithm(points);
+        ArrayList<Point> outPoints = qh.getHullPoints();
+
+        assertEquals(4, outPoints.size());
+    }
+
+    /**
+     * Test if four points placed as a rectangle return
+     * four hull points.
+     */
+    @Test
+    public final void check2DForSomeHullPointsWithInnerPoints() {
+        ArrayList<Point> points = new ArrayList<Point>();
+
+        // outer points
+        points.add(new Point(0, 0));
+        points.add(new Point(10, 0));
+        points.add(new Point(10, 10));
+        points.add(new Point(0, 10));
+
+        // inner points (diagonal)
+        points.add(new Point(2, 2));
+        points.add(new Point(3, 3));
+        points.add(new Point(4, 4));
+
+        // inner points (somewhere)
+        points.add(new Point(5, 2));
+        points.add(new Point(1, 9));
+        points.add(new Point(9, 8));
+
+        QuickHullAlgorithm qh = new QuickHullAlgorithm(points);
+        ArrayList<Point> outPoints = qh.getHullPoints();
+
+        // only four points must be in the hull list
+        assertEquals(4, outPoints.size());
+    }
+}
